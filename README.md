596dd93a2b2634232e3a5d41e8744fc28c2cfa5b
Binatik <47430210+Binatik@users.noreply.github.com>
2021-01-19 20:18:33 +0300

    add gulp

---
+++ README.md
@@ -0,0 +1,30 @@
+# Сборка gulp
+[![N|Solid](https://i.ibb.co/NCvVBt8/gulp-init.png)]()  
+
+## Команды первичной установки:
+```sh 
+npm install --global gulp-cli
+npm init 
+npm install gulp --save-dev
+``` 
+
+### Пакеты:
+| Package | Description |
+| ------ | ------ |
+| npm i browser-sync gulp --save-dev |[Синхронизация окон браузеров](https://browsersync.io/"")| 
+| npm i gulp-file-include --save-dev |[Дает возможность инклудить блоки](https://www.npmjs.com/package/gulp-file-include"")| 
+| npm i gulp-autoprefixer --save-dev |[Auto autoprefixer CSS](https://www.npmjs.com/package/gulp-autoprefixer"")| 
+| npm i gulp-group-css-media-queries --save-dev |[#media запросы группирует в один из нескольких повторений](https://www.npmjs.com/package/gulp-group-css-media-queries"")| 
+| npm i gulp-clean-css --save-dev |[Оптимизирует css код](https://www.npmjs.com/package/gulp-clean-css"")| 
+| npm i gulp-rename --save-dev |[Изменяет имя выходного файл формата .name. Типа .min.css](https://www.npmjs.com/package/gulp-rename"")| 
+| npm i gulp-uglify --save-dev |[Уменьшает размер JavaScript](https://www.npmjs.com/package/gulp-uglify"")| 
+| npm install --save-dev gulp-babel @babel/core @babel/preset-env |[@babel, jsx](https://babeljs.io/"")|  
+| npm i gulp-imagemin --save-dev |[Уменьшает размер изображения и оптимизирует их](https://www.npmjs.com/package/gulp-imagemin"")| 
+| npm i gulp-webp --save-dev |[Кодирует изображение в современный формат](https://www.npmjs.com/package/gulp-webp"")| 
+| npm i del --save-dev |[Clean](https://www.npmjs.com/package/del"")|  
+
+### Установка всех package.json:
+```sh 
+npm i
+``` 
+
---
+++ gulpfile.js
@@ -0,0 +1,161 @@
+const formats = '{img,svg,webp,jpg,png}';
+//Получаем пути для чтения  объектов.
+const getPaths = (root) => ({
+    html: [`${root}/*.html`, `!${root}/+*.html`],
+    image: `${root}/image/**/*.${formats}`,
+    fonts: `${root}/fonts/*.ttf`,
+    styles: [`${root}/styles/*.css`, `!${root}/styles/+*.css`],
+    scripts: [`${root}/scripts/*.js`, `!${root}/scripts/+*.js`],
+
+});
+
+//Удаления проекта из папки ways
+function pathDelete(ways) {
+    return `./${ways}/`
+}
+
+function compressPhoto(level) {
+    return imagemin({
+        progressive: true,
+        interlaced: true,
+        optimizationLevel: level,
+    })
+}
+
+//Выполняем task 
+function getTask(task) {
+    if (task === 'html') {
+        return src(getPaths('development').html)
+            .pipe(fileinclude({ 
+                prefix: '-',
+            }))
+            .pipe(webpHTML())
+    }
+
+    if (task === 'image') {
+        return src(getPaths('development').image)
+            .pipe(compressPhoto(3))
+            .pipe(dest(`build/image/`))
+            .pipe(webp({
+                quality: 75,
+            }))
+            .pipe(compressPhoto(3))
+    }
+
+    if (task === 'fonts') {
+        return src(getPaths('development').fonts)
+    }
+
+    if (task === 'styles') {
+        return src(getPaths('development').styles)
+            .pipe(fileinclude({ 
+                prefix: '@',
+            }))
+            .pipe(autoprefixer({
+                overrideBrowserslist: ['last 5 versions'],
+                cascade: true
+            }))
+            .pipe(dest(`build/styles/`))
+            .pipe(gcmq())
+            .pipe(cleanCSS({
+                compatibility: 'ie8'
+            }))
+            .pipe(rename({
+                extname: '.min.css'
+            }))
+    }
+    if (task === 'scripts') {
+        return src(getPaths('development').scripts)
+            .pipe(fileinclude({ 
+                prefix: '-',
+            }))
+            .pipe(dest(`build/scripts/`))
+            .pipe(babel({
+                presets: ['@babel/env'], 
+                plugins: ['transform-react-jsx']
+            }))
+            .pipe(uglify())
+            .pipe(rename({
+                extname: '.min.js'
+            }))
+
+    }
+}
+
+//Подключаем  gulp.
+const {
+    src,
+    dest
+} = require('gulp'),
+
+    gulp = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    fileinclude = require('gulp-file-include'),
+    autoprefixer = require('gulp-autoprefixer'),
+    gcmq = require('gulp-group-css-media-queries'),
+    cleanCSS = require('gulp-clean-css'),
+    rename = require("gulp-rename"),
+    uglify = require('gulp-uglify'),
+    babel = require('gulp-babel'),
+    imagemin = require('gulp-imagemin'),
+    webp = require('gulp-webp'),
+    webpHTML = require('gulp-webp-html'),
+    del = require('del');
+
+function updatePage() {
+    browserSync.init({
+        server: {
+            baseDir: './build/'
+        },
+    })
+
+    gulp.watch(`development/**/*.html`, getHtml);
+    gulp.watch(`development/image/**/*.${formats}`, getImage);
+    gulp.watch(`development/**/*.css`, getStyles);
+    gulp.watch(`development/**/*.js`, getScripts);
+}
+
+
+
+
+
+//Методы
+const delite = () => del(pathDelete('build'));
+
+function build(func = '', name, dirname = '', ) {
+    return func()
+        .pipe(dest(`build/${dirname}`))
+        .pipe(src(getPaths(`build/${dirname ? `${dirname}/*.*` : ''}`)[name]))
+        .pipe(browserSync.stream())
+}
+
+
+function getHtml() {
+    return build(() => getTask('html'), 'html');
+}
+
+function getImage() {
+    return build(() => getTask('image'), 'image', 'image');
+}
+
+function getFonts() {
+    return build(() => getTask('fonts'), 'fonts', 'fonts');
+}
+
+function getStyles() {
+    return build(() => getTask('styles'), 'styles', 'styles');
+}
+
+function getScripts() {
+    return build(() => getTask('scripts'), 'scripts', 'scripts');
+}
+
+
+
+
+
+//Комбинации.  
+const defaultGulp = gulp.series(delite, getHtml, getImage, getFonts, getStyles, getScripts, updatePage);
+
+//module exports.arbitraryName = function/variable 
+exports.default = defaultGulp;
