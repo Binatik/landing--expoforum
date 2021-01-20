03e7ce04f67c52ec9c0f8f2e7733d215a8fad3bb
Сергей Александрович <47430210+Binatik@users.noreply.github.com>
2021-01-20 22:09:51 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,26 @@
 function Search() {
+    const searchContainer = document.querySelector('.expoforum__search');
     const searchInput = document.querySelector('.expoforum__search-input');
     const searchSvg = document.querySelector('.expoforum__search-svg');
 
     searchSvg.addEventListener("click", (event) => {
         searchSvg.classList.toggle('expoforum__search-svg_active');
         searchInput.classList.toggle('expoforum__search-input_active');
+        searchContainer.classList.toggle('expoforum__search_active');
     });
 }
 
 Search();
  
+
+function Burger() {
+    const menu = document.querySelector('.mobile__menu');
+    const burgerSvg = document.querySelector('.mobile__burger');
+
+    burgerSvg.addEventListener("click", (event) => {
+        burgerSvg.classList.toggle('burger_active');
+        menu.classList.toggle('mobile__menu_active');
+    });
+}
+
+Burger();
