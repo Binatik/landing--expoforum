7f34e9ec3e172110163f1accc09ae6ecdc9d32ac
Сергей Александрович <47430210+Binatik@users.noreply.github.com>
2021-01-21 02:18:15 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,49 @@
+/* throttle */
+(function() {
+    const throttle = function(type, name, obj) {
+        obj = obj || window;
+        let running = false;
+        const func = function() {
+            if (running) { return; }
+            running = true;
+            requestAnimationFrame(function() {
+                obj.dispatchEvent(new CustomEvent(name));
+                running = false;
+            });
+        };
+        obj.addEventListener(type, func);
+    };
+    /* init - you can init any event */
+    throttle("resize", "optimizedResize");
+})();
+
+function throttleFn(fn, delay) {
+    let lastCall = 0;
+
+    return function(...args) {
+        const now = Date.now();
+
+        if (now - lastCall < delay) {
+            return;
+        }
+
+        lastCall = now;
+        fn.apply(this, args);
+    };
+}
+
+window.addEventListener("optimizedResize", (event) => {
+    const heightNav = document.querySelector('.expoforum__navigation').offsetHeight;
+    const bar = document.querySelector('.expoforum__bar');
+    if (window.innerWidth > 768) {
+        bar.setAttribute('style', `height: ${heightNav}px`);
+        console.log(heightNav);
+    }
+
+    else {
+        bar.setAttribute('style', `height: 100%`);
+    }
+})
 function Search() {
     const searchContainer = document.querySelector('.expoforum__search');
     const searchInput = document.querySelector('.expoforum__search-input');
